dac30f3d50f55db8e9850876acbf55852f55dd7b
clooudbase <clooudbase@gmail.com>
2023-08-08 22:48:30 +0400

    first commit

---
+++ src/Like.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Eslamfaroug\LaravelLikeDislike;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    protected $fillable = [
+        'liker_id', 'liker_type', 'likeable_id', 'likeable_type', 'type',
+    ];
+    const LIKE = 'like';
+    const DISLIKE = 'dislike';
+
+    public function likeable()
+    {
+        return $this->morphTo('likeable');
+    }
+
+    public function liker()
+    {
+        return $this->morphTo('liker');
+    }
+
+    public function scopeWithType($query, $type)
+    {
+        return $query->where('type', $type);
+    }
+}
+
---
+++ src/LikeSystemServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Eslamfaroug\LaravelLikeDislike;
+
+use Eslamfaroug\LaravelLikeDislike\Repositories\LikeSystemRepository;
+use Eslamfaroug\LaravelLikeDislike\Services\LikeSystemService;
+use Illuminate\Support\ServiceProvider;
+
+class LikeSystemServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('like-system', function ($app) {
+            return new LikeSystemService(new LikeSystemRepository());
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish migration
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
+
+        // Publish configuration
+        $this->publishes([
+            __DIR__.'/../config/like-system.php' => config_path('like-system.php'),
+        ], 'config');
+    }
+
+}
